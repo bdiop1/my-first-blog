d6bec7aa1d22e43f8e573b80fd7f410f5928ef76
bdiop1 <39636856+bdiop1@users.noreply.github.com>
2018-05-26 12:32:56 +0300

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -12,10 +12,17 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+Bi moy marcher ak django 2
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+]
+"""
+from django.conf.urls import url
+from django.contrib import admin
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
 ]
